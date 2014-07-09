fcedbf081f1f7441dcb4120fc558cd5371741753
Mohammad Anwari <mdamt@mdamt.net>
2014-07-09 10:20:48 +0700

    Sync user db in admin seed

--- resources/user/fixtures/seed.js
+++ resources/user/fixtures/seed.js
@@ -4,22 +4,40 @@ mongoose.connect ("mongodb://localhost/sureliaAdmin");
 
 process.env.CRYPTO_KEYS = ["admin", "keys"];
 var schemas = require ("../schemas");
+var domainSchemas = require ("../../domain/schemas");
+
 var User = schemas.User;
+var Domain = domainSchemas.Domain;
 
-User.remove(function(){
-  var user = {
-    email : "admin@example.com",
-    password : "12345",
-    name : "Admin",
-    roles : ["admin"]
-  };
 
-  User.register(user, function(err, registered){
-    if (err) throw err;
-    User.activate(registered.secret, function(err, activated){
-      if (err) throw err;
-      console.log ("admin@example.com is created", user.password);
-      mongoose.disconnect();
+Domain.remove(function(){
+  User.remove(function(){
+    var domain = {
+      name : "example.com",
+      state : "active",
+      creator : "000000000000000000000000",
+      createdDate : new Date
+    }
+
+    var user = {
+      username: "admin",
+      password : "12345",
+      name : "Admin",
+      mailboxServer : "000000000000000000000000",
+      roles : ["admin"]
+    };
+
+    Domain.create(domain, function(err, domainData) { 
+      user.domain = domainData._id;
+      User.register(user, function(err, registered){
+      console.log(err);
+        if (err) throw err;
+        User.activate(registered.secret, function(err, activated){
+          if (err) throw err;
+          console.log ("admin@example.com is created", user.password);
+          mongoose.disconnect();
+        });
+      });
     });
   });
 });
