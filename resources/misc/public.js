b2ff26ee9144b8bda8d967f34a29ba627bab273d
Mohammad Anwari <mdamt@mdamt.net>
2016-04-09 11:57:11 +0700

    Always get last uploaded logo

--- resources/misc/public.js
+++ resources/misc/public.js
@@ -14,13 +14,13 @@ var options;
 
 service.get("/public-api/1/province", function *() {
   var self = this;
-  
+
   var obj = yield Province
     .find()
     .sort({num : 1})
     .lean()
-    .exec(); 
-  self.body = { 
+    .exec();
+  self.body = {
     type: "list",
     data: obj
   };
@@ -28,13 +28,13 @@ service.get("/public-api/1/province", function *() {
 
 service.get("/public-api/1/kab-kota", function *() {
   var self = this;
-  
+
   var obj = yield KabKota
     .find()
     .sort({name : 1})
     .lean()
-    .exec(); 
-  self.body = { 
+    .exec();
+  self.body = {
     type: "list",
     data: obj
   };
@@ -43,13 +43,13 @@ service.get("/public-api/1/kab-kota", function *() {
 service.get("/public-api/1/kab-kota/:id", function *() {
   var self = this;
   var id = self.params.id;
-  
+
   var obj = yield KabKota
     .find({province: id})
     .sort({name : 1})
     .lean()
-    .exec(); 
-  self.body = { 
+    .exec();
+  self.body = {
     type: "list",
     data: obj
   };
@@ -57,28 +57,50 @@ service.get("/public-api/1/kab-kota/:id", function *() {
 
 
 service.get("/public-api/1/logo/:domain", function *() {
+
+  function findLast() {
+    return new Promise((resolve, reject) => {
+      gfs.files.find({filename: domain}).sort({uploadDate: -1}).toArray((err, files)=> {
+        if (err) {
+          reject(err);
+          return;
+        }
+        if (!files || files.length == 0) {
+          reject(new Error());
+          return;
+        }
+        resolve(files[0]._id);
+      });
+    });
+  }
   var self = this;
   var domain = self.params.domain;
   var rs = Readable();
   var ws = Writable();
   rs._read = function(){};
-  ws._write = function(chunk, enc, next){      
+  ws._write = function(chunk, enc, next){
     rs.push(chunk);
     next();
   }
-  var stream = gfs.createReadStream({filename : domain});
-  stream.on("error", function(err) {
-    self.status = err.status || 404;
-    self.body = err;
-    rs.push(null);
-  })
-  stream.pipe(ws);
-  stream.on("end", function(){
-    rs.push(null);
-  });
-  self.type = "text/plain"
-  self.body = rs.pipe(base64Stream.encode());
-  onFinished(self, stream.destroy.bind(stream));
+
+  function getLogo(id) {
+    var stream = gfs.createReadStream({filename : domain, _id: id});
+    stream.on("error", function(err) {
+      self.status = err.status || 404;
+      self.body = err;
+      rs.push(null);
+    })
+    stream.pipe(ws);
+    stream.on("end", function(){
+      rs.push(null);
+    });
+    self.type = "text/plain"
+      self.body = rs.pipe(base64Stream.encode());
+    onFinished(self, stream.destroy.bind(stream));
+  }
+
+  var id = yield findLast();
+  getLogo(id);
 });
 
 
